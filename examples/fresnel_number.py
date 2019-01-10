cedf9776f8a34c019cdf201184562a3d01b5d1ef
Manuel Sanchez del Rio <srio@esrf.eu>
2019-01-10 23:20:43 +0100

    try

---
+++ examples/fresnel_number.py
@@ -0,0 +1,27 @@
+import numpy
+import scipy.constants as codata
+from srxraylib.plot.gol import plot
+
+
+energy = 10000.0
+wavelength =  ( codata.h*codata.c/codata.e*1e9 /energy)*10**(-9)
+window_size = 5e-6
+aperture_diameter = 1.25e-06
+propagation_distance = 75e-6
+N = 2048
+
+delta = window_size/N
+propagation_distance = numpy.linspace(0,1000e-6,100)
+#
+# info
+#
+# print("Dz <? N d^2 / lambda; %g < %g " %(propagation_distance, window_size*0.5 * delta/wavelength))
+# print("Fresnel number: xmaxD / (lambda Dz) = %g  " %(0.5*window_size /(propagation_distance * wavelength)))
+
+edge = window_size*0.5 * delta /wavelength
+fresnel = 0.5*window_size /(propagation_distance * wavelength)
+
+plot(1e6*propagation_distance,propagation_distance,
+     1e6*propagation_distance,propagation_distance*0 + edge)
+
+plot(propagation_distance,fresnel,title="Fresnel number")
